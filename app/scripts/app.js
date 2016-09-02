ee12d4b960bd13035cc2ea6a1e705fc54287e6fb
bruce <bruce.jcw@gmail.com>
2016-09-02 10:22:27 +0800

    opt extract ip

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,9 +26,16 @@ function getLocalIPs(callback) {
             callback(ips);
             return;
         }
-        var ip = /^candidate:.+ (\S+) \d+ typ/.exec(e.candidate.candidate)[1];
-        if (ips.indexOf(ip) == -1) // avoid duplicate entries (tcp/udp)
+        /**
+         * get candidate which endwith 'network-id 1', it should be the right ip
+         */
+        var result = /^candidate:.+ (\S+) \d+ typ .* network-id 1/.exec(e.candidate.candidate);
+        if (result){
+          var ip = result[1]
+          if (ips.indexOf(ip) == -1){ // avoid duplicate entries (tcp/udp)
             ips.push(ip);
+          }
+        }
     };
     pc.createOffer(function(sdp) {
         pc.setLocalDescription(sdp);
